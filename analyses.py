d844d1096020572a0eee24d2e11bfac752bfde1e
Willie2511 <60076968+Willie2511@users.noreply.github.com>
2020-04-17 12:03:30 +0100

    Add files via upload

--- analyses.py
+++ analyses.py
@@ -75,10 +75,19 @@ pyplot.savefig('ScatterPlot.png')
 
 
 
+array = dataset.values
+X = array[: ,0:4]
+Y = array[: ,4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation =
+model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+seed = 7
+scoring = 'accuracy'
 
 
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-
-
-
+###############################
 
