f1c79f6bf1a3880cb83e1e205a72299597358e60
Willie2511 <60076968+Willie2511@users.noreply.github.com>
2020-04-17 14:49:38 +0100

    Add files via upload

--- analyses.py
+++ analyses.py
@@ -2,7 +2,11 @@
 # Analyses of Fisher's Iris Data set
 # First section will import, analyse and produce graphs
 # to visually represent the data.
-# Second section will attempt to take on a 
+# Second section will interpret the data from resulting graphs 
+# and tables, in order to draw some definitive conclusions
+
+
+# Import all libraries which will be needed to analyses the dataset
 
 import sys
 import scipy
@@ -10,65 +14,25 @@ import numpy as np
 import matplotlib 
 import pandas
 import sklearn
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
 from pandas import read_csv
 from matplotlib import pyplot
-
-
-# load libraries
-from pandas import read_csv
 from pandas.plotting import scatter_matrix
-from pandas.api.types import is_numeric_dtype 
-from matplotlib import pyplot
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC 
 
 
-# Experimental 
-import pandas
-import matplotlib.pyplot as plt
-from sklearn import model_selection
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
+# First we Load dataset and use the 'read_csv' function from pandas library to 
+# enable us to use a .csv file. 
+# Names are given for each variable which is included in the dataset, which will
+# be used to differentiate the different flowers.
 
-
-
-# Load dataset
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
 
 
-# Shape
+# Print the shape of the data by using the 'dataset.shape' function.
+# Returns tuple of shape of dataset.
+
 print (dataset.shape)
-
-#Calculating average, standard deviation, minimum and maximum values
-#for col in names:
- #   if is_numeric_dtype(dataset[col]):
-  #      print('%s:' % (col), file=open("Flowers_summary.txt", 'w'))
-   #     print('\t Mean = %.2f' % dataset[col].mean(), file=open("Flowers_summary.txt", 'w'))
-    #    print('\t Standard deviation = %.2f' % dataset[col].mean(), file=open("Flowers_summary.txt", 'w'))
-     #   print('\t Minimum = %.2f' % dataset[col].min(), file=open("Flowers_summary.txt", 'w'))
-      #  print('\t Maximum = %.2f' % dataset[col].max(), file=open("Flowers_summary.txt", 'w'))
-
 
 # Head
 print (dataset.head(20))
